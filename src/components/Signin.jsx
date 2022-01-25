a5f8501ead2f39dec39bed9efa7a1faafd87c15a
Gandalf925 <theengineerofoz@gmail.com>
2022-01-25 23:17:40 +0900

    setName by clicking button

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -47,6 +47,15 @@ const useStyles = makeStyles((theme) => ({
 export default function SignIn(setName) {
   const classes = useStyles();
 
+  const [disabled, setDisabled] = useState(true);
+  const [string, setString] = useState('');
+  console.log(disabled, string);
+
+  useEffect(() => {
+    const disabled = string === '';
+    setDisabled(disabled);
+  }, [string]);
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -64,6 +73,7 @@ export default function SignIn(setName) {
             label="ニックネーム"
             name="email"
             autoFocus
+            onChange={(e) => setString(e.target.value)}
           />
           <Button
             type="submit"
@@ -71,6 +81,7 @@ export default function SignIn(setName) {
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled={disabled}
           >
             はじめる
           </Button>
